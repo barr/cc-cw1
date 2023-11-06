c5b6c79ed2b9ccb7378de1d2117396634020cabf
Yingbo Fu <Yingbo.Fu.21@ucl.ac.uk>
2023-11-06 20:23:11 +0000

    adapt to tree-sitter path

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "export PATH=$PATH:./node_modules/.bin" >> $HOME/.bashrc
+echo "export PATH=$PATH:./tree-sitter-YOUR_LANGUAGE_NAME/node_modules/.bin" >> $HOME/.bashrc
 source $HOME/.bashrc
